33b26dca0132d41a918ace81020060105fa3728d
alex pitzen <alexpitzen@gmail.com>
2023-09-26 23:51:01 -0500

    Reset orientation

--- laptop_mode.sh
+++ laptop_mode.sh
@@ -5,4 +5,11 @@ killall tablet_mode.sh
 echo serio1 > /sys/bus/serio/drivers/psmouse/bind
 echo serio0 > /sys/bus/serio/drivers/atkbd/bind
 
+# temp disable accelerometer
+echo '0020:1022:0001.0001' > /sys/bus/hid/drivers/hid-sensor-hub/unbind
+
+# tablet mode off & reset orientation
 python3 tablet_mode.py 0
+
+# reenable accelerometer
+echo '0020:1022:0001.0001' > /sys/bus/hid/drivers/hid-sensor-hub/bind
--- tablet_mode.py
+++ tablet_mode.py
@@ -1,5 +1,6 @@
 import sys
-from evdev import UInput, ecodes as e, InputEvent
+from time import sleep
+from evdev import UInput, ecodes as e, InputEvent, AbsInfo
 
 def set_tablet_mode(mode: int):
     """
@@ -9,12 +10,27 @@ def set_tablet_mode(mode: int):
     assert mode in [0, 1], "mode must be in [0, 1]"
 
     cap = {
-        e.EV_SW: [e.SW_TABLET_MODE]
+        e.EV_SW: [e.SW_TABLET_MODE],
+        e.EV_ABS: [
+            (e.ABS_X, AbsInfo(0, -512, 512, 0, 0, 0)),
+            (e.ABS_Y, AbsInfo(0, -512, 512, 0, 0, 0)),
+            (e.ABS_Z, AbsInfo(0, -512, 512, 0, 0, 0)),
+        ],
     }
 
     current_mode = InputEvent(1334414993, 274296, e.EV_SW, e.SW_TABLET_MODE, 1 - mode)
     new_mode = InputEvent(1334414993, 274296, e.EV_SW, e.SW_TABLET_MODE, mode)
     with UInput(cap) as ui:
+        if mode == 0:
+            # TODO do something better than guessing when iio-sensor-proxy will register our input
+            sleep(0.5)
+            ui.write_event(InputEvent(1334414993, 274296, e.EV_ABS, e.ABS_X, 0))
+            ui.write_event(InputEvent(1334414993, 274296, e.EV_ABS, e.ABS_Y, -256))
+            ui.write_event(InputEvent(1334414993, 274296, e.EV_ABS, e.ABS_Z, 0))
+            ui.syn()
+            # TODO do something better than guessing when iio-sensor-proxy will register our event
+            sleep(0.5)
+
         # Setting back to laptop mode doesn't work if we don't
         # first set to tablet mode again for some reason
         ui.write_event(current_mode)
